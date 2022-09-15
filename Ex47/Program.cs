31385af07212392dc240672c79cd187324b371ed
Katya <katvas785@gmail.com>
2022-09-15 19:23:08 +0300

    Ex47

---
+++ Ex47/Program.cs
@@ -0,0 +1,14 @@
+﻿int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[m,n];
+
+for (int i = 0; i<array.GetLength(0); i++)
+{
+    for(int j=0; j<array.GetLength(1); j++)
+    {
+        array[i,j] = new Random().Next(1,100);
+        Console.Write($"{array[i,j]} ");
+    }
+  Console.WriteLine();
+}
+ 
